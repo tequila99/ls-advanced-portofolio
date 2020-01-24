5e7f23ed53cd310b797082619761fa81f1a0995a
ivan_milovanov <tequila00@yandex.ru>
2020-01-24 18:41:40 +0300

    Add main-parallax

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
 import "./styles/main.pcss";
+
+import parallax from "./scripts/parallax.js";
+
 if (process.env.NODE_ENV === "development") {
   require("file-loader!./index.pug");
 }
 
 import "./scripts/skills";
+
+let mainParallaxItems = Array.prototype.slice.call(document.querySelectorAll(".parralax-scene--main .js-parallax-item"));
+
+window.addEventListener("wheel", () => {
+	let scroll = window.pageYOffset;
+
+  // main parallax
+	if (scroll < window.innerHeight) {
+		mainParallaxItems.forEach(block => {
+			let accel = block.dataset.speed;
+
+			parallax.strafe(block, scroll, accel);
+		});
+  }
+});
